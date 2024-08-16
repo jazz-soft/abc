613b612c26b7095046edbbea8651626fb9929066
Sema <jazz-dev@jazz-soft.net>
2024-08-15 21:05:51 -0400

    EditorView.decorations

--- abc.mjs
+++ abc.mjs
@@ -1,6 +1,6 @@
 import {EditorView, basicSetup} from "codemirror";
 import {Decoration, ViewPlugin} from "@codemirror/view";
-import {StateField} from "@codemirror/state";
+import {StateField, StateEffect} from "@codemirror/state";
 
 import JZZ from "jzz";
 import ABC from "jazz-abc";
@@ -25,39 +25,56 @@ midi_in.select();
 midi_out.select();
 
 const theme = EditorView.baseTheme({
-  ".cm-comment": { textDecoration: "underline 3px red" }
+  ".cm-field": { color: "blue", fontWeight: "bold" },
+  ".cm-comment": { color: "green", fontStyle: "italic" }
 });
 
+const fieldMark = Decoration.mark({class: "cm-field"});
 const commentMark = Decoration.mark({class: "cm-comment"});
 
 const watcher = EditorView.updateListener.of((update) => {
   if (update.docChanged) {
+    //console.log(update.state.field(parser));
   }
 });
 
-const decorator = StateField.define({
-  create() {
-    return Decoration.set([]);
-  },
+const parser = StateField.define({
+  create() {},
   update(val, tr) {
     if (!tr.docChanged) return val;
-    return Decoration.set([
-      commentMark.range(0, 1)
-    ]);
+    return new ABC.Parser(tr.state.doc.text.join('\n'));
+  }
+});
+
+function decorate(a, d, t) {
+  if (t.t == undefined) return;
+  var mark;
+  if (t.t[1] == ':') mark = fieldMark;
+  else if (t.t == '%') mark = commentMark;
+  if (mark) {
+    var from = d.line(t.l + 1).from + t.c;
+    var to = from + t.x.length;
+    a.push(mark.range(from, to));
+  }
+}
+
+const decorator = StateField.define({
+  create() { return Decoration.set([]); },
+  update(val, tr) {
+    if (!tr.docChanged) return val;
+    const data = tr.state.field(parser).data;
+    const dec = [];
+    for (var line of data) for (var token of line) decorate(dec, tr.state.doc, token);
+    return Decoration.set(dec);
   },
   provide: (x) => EditorView.decorations.from(x)
 });
 
 let editor = new EditorView({
-  extensions: [basicSetup, theme, watcher, decorator],
+  extensions: [basicSetup, theme, parser, decorator, watcher],
   parent: document.body
 })
 
 widget._receive = function(msg) {
   if (!msg.isNoteOn()) return;
 };
-
-function tokenize(s) {
-  var p = new TAB.Parser(s);
-  return p.tok;
-}
